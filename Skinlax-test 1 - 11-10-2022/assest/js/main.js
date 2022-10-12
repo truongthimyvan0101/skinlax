8ce47a88e1ce130415eae3bde3cf4f2e0fbb6790
unknown <you@example.com>
2022-10-12 14:39:50 +0700

    up

---
+++ Skinlax-test 1 - 11-10-2022/assest/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.menu-nav .menu').click(function(){
+    $('.menu-nav .menu').toggleClass('active')
+    $('#head .social .list-item').slideToggle('slow');
+  })
+  
+//   $('#header .navbar .m-menu .hasSub a i').click(function(){
+//     $('#header .navbar .m-menu .hasSub .subMenu').slideToggle('slow');
+//   })
+ 
+     });
